69b9d19b4f1980ad5d014f315e2a9fb16db59287
Sumukh Moudgalya <52463722+sumukh-moudgalya@users.noreply.github.com>
2020-04-19 19:13:17 +0530

    Update mode_1_16_bit_asm.asm

--- timers/MODE 1 16 BIT USING ASL/mode_1_16_bit_asm.asm
+++ timers/MODE 1 16 BIT USING ASL/mode_1_16_bit_asm.asm
@@ -2,55 +2,36 @@
 ORG 0000H;
 
  
-MOV TMOD,#01H									;selecting 16-bit timer i.e, mode 1 
+MOV TMOD,#01H;selecting 16-bit timer i.e, mode 1 
 
-LOOP1:													;running the program continously 
+LOOP1:;running the program continously 
 
-MOV P1,#0FFH											;logic high at port 1 for square wave
+MOV P1,#0FFH;logic high at port 1 for square wave
 
-ACALL DELAY											;providing a delay of 5ms
+ACALL DELAY;providing a delay of 5ms
 
-MOV P1,#0H												;logic low at port 1 for square wave
+MOV P1,#0H;logic low at port 1 for square wave
 
-ACALL DELAY												;providing a delay of 5ms
+ACALL DELAY;providing a delay of 5ms
 
-SJMP LOOP1												;calling the loop again
+SJMP LOOP1;calling the loop again
 
-DELAY:													;defining the subroutine delay
+DELAY:;defining the subroutine delay
 
-SETB TR0												;selecting timer 0
+SETB TR0;selecting timer 0
 
-MOV TL0,#0H												;setting the data of TL0 to 0
+MOV TL0,#0H;setting the data of TL0 to 0
 
-MOV TH0,#0EEH											;setting the data of TH0 to EE
+MOV TH0,#0EEH;setting the data of TH0 to EE
 
 LOOP2:
 
-JNB TF0,LOOP2										;jump if TF0 bit is not set to LOOP2
+JNB TF0,LOOP2;jump if TF0 bit is not set to LOOP2
 
-CLR TR0														;deselect the timer 0
+CLR TR0;deselect the timer 0
 
-CLR TF0												;setting the overflow flag of timer to 0 for using it again
+CLR TF0;setting the overflow flag of timer to 0 for using it again
 
-RET															;return
+RET;return
 
 END;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
